9f4afb82192f653df0de9ae040a1c601aa633d8c
Marlin Zapp <s4mazapp@uni-trier.de>
2026-04-03 14:30:43 +0200

    some trying later

--- src/main/java/com/example/merkletree/dss/Playground.java
+++ src/main/java/com/example/merkletree/dss/Playground.java
@@ -8,12 +8,10 @@ import eu.europa.esig.dss.model.FileDocument;
 import eu.europa.esig.dss.model.SignatureValue;
 import eu.europa.esig.dss.model.ToBeSigned;
 import eu.europa.esig.dss.model.x509.CertificateToken;
-import eu.europa.esig.dss.service.http.commons.TimestampDataLoader;
 import eu.europa.esig.dss.service.tsp.OnlineTSPSource;
 import eu.europa.esig.dss.spi.DSSUtils;
 import eu.europa.esig.dss.spi.validation.CommonCertificateVerifier;
 import eu.europa.esig.dss.spi.x509.CommonTrustedCertificateSource;
-import eu.europa.esig.dss.spi.x509.KeyStoreCertificateSource;
 import eu.europa.esig.dss.token.DSSPrivateKeyEntry;
 import eu.europa.esig.dss.token.Pkcs12SignatureToken;
 import eu.europa.esig.dss.xades.XAdESTimestampParameters;
@@ -24,8 +22,6 @@ import java.io.File;
 import java.security.KeyStore.PasswordProtection;
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.xml.crypto.dsig.CanonicalizationMethod;
 
 public class Playground {
     public Playground() {
@@ -41,22 +37,12 @@ public class Playground {
             // Get the first private key entry
             DSSPrivateKeyEntry privateKey = signingToken.getKeys().get(0);
 
-            KeyStoreCertificateSource myKeystore = new KeyStoreCertificateSource(
-                    new File("src/test/resources/test.p12"),
-                    "PKCS12", "changeit".toCharArray());
-
             CertificateToken tsaCertificate = DSSUtils
                     .loadCertificate(new File("src/test/resources/certificates/PN_Zeitstempel_2023.cer"));
 
             CommonTrustedCertificateSource trustedCertificateSource = new CommonTrustedCertificateSource();
             // Add the signing certificate explicitly as trusted (for self-signed certs)
             trustedCertificateSource.addCertificate(privateKey.getCertificate());
-
-            // Add all certificates from the keystore as trusted
-            for (CertificateToken cert : myKeystore.getCertificates()) {
-                trustedCertificateSource.addCertificate(cert);
-            }
-
             // Add the TSA certificate
             trustedCertificateSource.addCertificate(tsaCertificate);
 
@@ -70,29 +56,23 @@ public class Playground {
 
             // Create ASiC-XAdES signature parameters
             ASiCWithXAdESSignatureParameters parameters = new ASiCWithXAdESSignatureParameters();
-            parameters.setSignatureLevel(SignatureLevel.XAdES_BASELINE_LTA);
-            // Set the ASiC container type (ASiC-S for simple container)
+            parameters.setSignatureLevel(SignatureLevel.XAdES_BASELINE_T);
             parameters.aSiC().setContainerType(ASiCContainerType.ASiC_E);
             parameters.setSigningCertificate(privateKey.getCertificate());
             parameters.setCertificateChain(privateKey.getCertificateChain());
-            parameters.setGenerateTBSWithoutCertificate(true);
 
-            // Create ArchiveTimestamp parameters
-            XAdESTimestampParameters archiveTimestampParameters = new XAdESTimestampParameters();
-            archiveTimestampParameters.setCanonicalizationMethod(CanonicalizationMethod.EXCLUSIVE);
-            archiveTimestampParameters.setDigestAlgorithm(digestAlgorithm);
-            parameters.setArchiveTimestampParameters(archiveTimestampParameters);
+            System.out.println(parameters.getSigningCertificate());
+
+            XAdESTimestampParameters timestampParameters = new XAdESTimestampParameters(digestAlgorithm);
+            parameters.setSignatureTimestampParameters(timestampParameters);
 
             // Create ASiC service with a properly configured CommonCertificateVerifier
             CommonCertificateVerifier certificateVerifier = new CommonCertificateVerifier();
             certificateVerifier.setTrustedCertSources(trustedCertificateSource);
-            certificateVerifier.setCheckRevocationForUntrustedChains(false); // Disable revocation check for self-signed
-                                                                             // certs
             ASiCWithXAdESService service = new ASiCWithXAdESService(certificateVerifier);
 
             final String tspServer = "https://zeitstempel.dfn.de/";
             OnlineTSPSource tspSource = new OnlineTSPSource(tspServer);
-            tspSource.setDataLoader(new TimestampDataLoader()); // uses the specific content-typ
             service.setTspSource(tspSource);
 
             // Sign the documents in three steps
@@ -101,10 +81,10 @@ public class Playground {
             DSSDocument signedContainer = service.signDocument(documentsToSign, parameters, signatureValue);
 
             // Save the signed container
-            signedContainer.save("target/signed_container.asics");
+            signedContainer.save("target/signed_container.asice");
 
             System.out.println("ASiC container with XAdES signatures created successfully!");
-            System.out.println("Saved to: target/signed_container.asics");
+            System.out.println("Saved to: target/signed_container.asice");
 
             signingToken.close();
         } catch (Exception e) {
